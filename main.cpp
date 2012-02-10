1ddbb73b52c57d5ed98a8d788abbb097a7c00279
Blaine Pace <blainepace@gmail.com>
2012-02-09 20:50:53 -0800

    Added a basic render window and check for a close event.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 
 int main() {
-  sf::Clock clock;
-  while(clock.GetElapsedTime() < 5.f) {
-    std::cout << clock.GetElapsedTime() << std::endl;
+//  sf::Clock clock;
+//  while(clock.GetElapsedTime() < 5.f) {
+//    std::cout << clock.GetElapsedTime() << std::endl;
+//  }
+
+  sf::Window App(sf::VideoMode(800, 600, 32), "SFML Window");
+
+  bool running = true;
+
+  sf::Event Event;
+  while(running) {
+    if(App.GetEvent(Event))
+      if(Event.Type == sf::Event::Closed)
+        running = false;
+
+    App.Display();
   }
 
   return 0;
