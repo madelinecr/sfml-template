5af2f29e7f6a4aa9ab8cb344f7c9b19f843f1b1d
Blaine Pace <blainepace@gmail.com>
2012-02-09 21:40:19 -0800

    Draws a spinning test cube.

--- main.cpp
+++ main.cpp
@@ -7,13 +7,61 @@ void setClear() {
   glClearColor(0.f, 0.f, 0.f, 0.f);
 }
 
+void drawCube(sf::Clock Clock) {
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glTranslatef(0.f, 0.f, -200.f);
+  glRotatef(Clock.GetElapsedTime() * 50, 1.f, 0.f, 0.f);
+  glRotatef(Clock.GetElapsedTime() * 30, 0.f, 1.f, 0.f);
+  glRotatef(Clock.GetElapsedTime() * 90, 0.f, 0.f, 1.f);
+
+  glBegin(GL_QUADS);
+
+    glVertex3f(-50.f, -50.f, -50.f);
+    glVertex3f(-50.f,  50.f, -50.f);
+    glVertex3f( 50.f,  50.f, -50.f);
+    glVertex3f( 50.f, -50.f, -50.f);
+
+    glVertex3f(-50.f, -50.f, 50.f);
+    glVertex3f(-50.f,  50.f, 50.f);
+    glVertex3f( 50.f,  50.f, 50.f);
+    glVertex3f( 50.f, -50.f, 50.f);
+
+    glVertex3f(-50.f, -50.f, -50.f);
+    glVertex3f(-50.f,  50.f, -50.f);
+    glVertex3f(-50.f,  50.f,  50.f);
+    glVertex3f(-50.f, -50.f,  50.f);
+
+    glVertex3f(50.f, -50.f, -50.f);
+    glVertex3f(50.f,  50.f, -50.f);
+    glVertex3f(50.f,  50.f,  50.f);
+    glVertex3f(50.f, -50.f,  50.f);
+
+    glVertex3f(-50.f, -50.f,  50.f);
+    glVertex3f(-50.f, -50.f, -50.f);
+    glVertex3f( 50.f, -50.f, -50.f);
+    glVertex3f( 50.f, -50.f,  50.f);
+
+    glVertex3f(-50.f, 50.f,  50.f);
+    glVertex3f(-50.f, 50.f, -50.f);
+    glVertex3f( 50.f, 50.f, -50.f);
+    glVertex3f( 50.f, 50.f,  50.f);
+
+  glEnd();
+}
+
 int main() {
-//  sf::Clock clock;
-//  while(clock.GetElapsedTime() < 5.f) {
-//    std::cout << clock.GetElapsedTime() << std::endl;
-//  }
+  sf::Clock Clock;
 
   sf::Window App(sf::VideoMode(800, 600, 32), "SFML Window");
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
+
+  // Setup a perspective projection
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(90.f, 1.f, 1.f, 500.f);
 
   sf::Event Event;
   while(App.IsOpened()) {
@@ -23,6 +71,7 @@ int main() {
 
     setClear();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    drawCube(Clock);
     App.Display();
   }
 
